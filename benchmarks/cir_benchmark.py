a97840b10bff8eb6509c14d5cc45edee668341c8
hamedhemati <hemati.hmd@gmail.com>
2023-01-23 14:36:16 +0100

    Add CIR benchmark generator

---
+++ benchmarks/cir_benchmark.py
@@ -0,0 +1,92 @@
+import torch
+
+from avalanche.benchmarks.utils.classification_dataset import \
+    classification_subset
+from avalanche.benchmarks import dataset_benchmark
+
+from utils.generic import set_random_seed
+
+
+def generate_benchmark(
+        seed,
+        train_set,
+        test_set,
+        n_classes=None,
+        n_e=None,
+        scenario_table=None,
+        first_occurrences=None,
+        n_samples_table=None,
+        indices_per_class=None,
+):
+    set_random_seed(seed)
+
+    # Sample indices for each class
+    selected_indices = {}
+
+    def sample_per_class(c):
+        selected_indices[c] = {}
+        indices_c = indices_per_class[c][torch.randperm(
+            len(indices_per_class[c]))]
+        exp_c = torch.where(n_samples_table[c] != 0)[0]
+        for e, n in zip(exp_c, n_samples_table[c][exp_c]):
+            indices_c = indices_c[torch.randperm(len(indices_c))]
+            selected = indices_c[:n]
+            selected_indices[c][e.item()] = selected
+
+    _ = [sample_per_class(c) for c in range(n_classes)]
+
+    # Create dataset for each experience
+    def create_dataset_exp_i(exp_i):
+        present_classes = torch.where(scenario_table[:, exp_i] != 0)[0]
+        all_indices_i = torch.cat([selected_indices[c.item()][exp_i]
+                                   for c in present_classes])
+        ds_i = classification_subset(train_set, indices=all_indices_i)
+
+        return ds_i, all_indices_i
+
+    stream_items = [create_dataset_exp_i(i) for i in range(n_e)]
+    train_datasets = [t[0] for t in stream_items]
+    samples_per_exp = [t[1] for t in stream_items]
+
+    # Create benchmark
+    benchmark = dataset_benchmark(
+        train_datasets=train_datasets,
+        test_datasets=[test_set],
+    )
+
+    # Benchmark Info
+    # - Scenario table, n-Samples table and n-TrainSet
+    benchmark.first_occurrences = first_occurrences
+    benchmark.scenario_table = scenario_table
+    benchmark.n_samples_table = n_samples_table
+    benchmark.n_trainset = len(train_set)
+
+    # - Samples and number of samples in each experience
+    benchmark.samples_per_exp = samples_per_exp
+    benchmark.n_samples_per_exp = [len(benchmark.samples_per_exp[i]) for i in
+                                   range(scenario_table.shape[1])]
+
+    # - Total number of samples
+    all_selected_indices = torch.cat(samples_per_exp)
+    benchmark.n_total_samples = len(all_selected_indices)
+
+    # - Classes in each experience:
+    benchmark.present_classes_in_each_exp = [
+        torch.where(benchmark.scenario_table[:, i])[0]
+        for i in range(n_e)
+    ]
+
+    # - Seen classes up to each experience
+    def classes_seen_sofar(i):
+        seen_classes = benchmark.present_classes_in_each_exp[:i + 1]
+        seen_classes = set(torch.cat(seen_classes).numpy())
+        seen_classes = torch.LongTensor(list(seen_classes))
+
+        return seen_classes
+
+    benchmark.seen_classes = [classes_seen_sofar(i) for i in
+                              range(len(benchmark.present_classes_in_each_exp))]
+    return benchmark
+
+
+__all__ = ["generate_benchmark"]
---
+++ utils/generic.py
@@ -0,0 +1,42 @@
+import random
+import numpy as np
+import torch
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def set_random_seed(seed):
+    print("Setting random seed: ", seed)
+    random.seed(seed)
+    torch.cuda.cudnn_enabled = False
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.enabled = False
+
+
+def plot_scenario(scenario_table):
+    """
+    :param scenario_table: a C x E tensor (C: # of classes, E: # of experiences)
+    :return:
+    """
+    fig, ax = plt.subplots(1, 1, figsize=(10, 6))
+    fig.tight_layout()
+
+    # Colors
+    n_classes = scenario_table.shape[0]
+    n_e = scenario_table.shape[1]
+    cmap = sns.color_palette("deep", n_classes+1)
+    cmap[0] = (1, 1, 1)
+
+    # Heat Map
+    table_plot = scenario_table.clone()
+    for i in range(table_plot.shape[0]):
+        idx = table_plot[i] > 0.0
+        table_plot[i][idx] = i + 1
+    sns.heatmap(table_plot, cbar=False, cmap=cmap, vmin=0, ax=ax,
+                xticklabels=[str(i) if i % 2 ==0 else '' for i in range(n_e) ],
+                yticklabels=False, linewidths=0.5)
+
+    ax.set_xlabel("Experience", fontsize=12)
+    ax.set_ylabel("Class", fontsize=12)
